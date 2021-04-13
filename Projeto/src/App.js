4a7bfb9338076acc5961e552ea12fae01e5533d0
ViictorSR388 <71882342+ViictorSR388@users.noreply.github.com>
2021-04-13 10:08:05 -0300

    Add files via upload

---
+++ Projeto/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [item, setItem] = useState('');
+
+  const [itemList, setItemList] = useState([])
+
+  const addItem = () => {
+    setItemList([item].concat(itemList))
+    setItem('')
+  }
+
+  <button onClick={addItem}>Adicionar Item</button>
+
+  return (
+    <div className="App">
+      <h1>Lista de Compras</h1>
+      <input type="text" placeholder="Item" value={item} name="item" onChange = {e => setItem(e.target.value)} />
+      <button onClick={addItem}>Adicionar Item</button>
+      <ul>
+        {itemList.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
